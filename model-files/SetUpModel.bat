9fec2626f141623c8db67010eb5270a0f32ab8f2
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2021-05-31 14:31:34 -0400

    Updated SetUpModel.bat, needed quote marks around GitHub path

--- model-files/SetUpModel.bat
+++ model-files/SetUpModel.bat
@@ -8,7 +8,7 @@
 SET computer_prefix=%computername:~0,4%
 
 :: copy over CTRAMP
-set GITHUB_DIR= F:\23791501\GitHub\travel-model-one
+set GITHUB_DIR= "F:\23791501\GitHub\travel-model-one"
 mkdir CTRAMP\model
 mkdir CTRAMP\runtime
 mkdir CTRAMP\scripts
