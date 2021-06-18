4be7c09bc597cd72e698dcde4a2b11d767352a0a
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2021-06-18 02:46:07 -0400

    Changes to generalise the restart run process for different scenarios
    In this case the changes correspond to scenario 1 (no changes to the base fares).

---
+++ model-files/LogRestartRunModel1.bat
@@ -0,0 +1,46 @@
+
+:: The index number of the scenario
+SET SCENARIOINDEX=1
+
+:: The name of the scenario and the name of the scenario folder in which the model will be run.
+SET SCENARIONAME=2015_TM152_STR_BA
+
+:: The path to the project folder
+SET PROJECTPATH=F:\23791501\
+
+:: The path for storing outputs
+SET OUTPUTSPATH=%PROJECTPATH%outputs\
+
+:: The path for storing logs
+SET LOGPATH=%PROJECTPATH%logs\
+
+:: The path for input fares files
+SET INPUTFARESPATH=%PROJECTPATH%inputs\%SCENARIOINDEX%
+
+copy /Y %INPUTFARESPATH%\ACE.far                            					%PROJECTPATH%%SCENARIONAME%\INPUT\trn
+copy /Y %INPUTFARESPATH%\Amtrak.far                          					%PROJECTPATH%%SCENARIONAME%\INPUT\trn
+copy /Y %INPUTFARESPATH%\BART.far                            					%PROJECTPATH%%SCENARIONAME%\INPUT\trn
+copy /Y %INPUTFARESPATH%\Caltrain.far                          					%PROJECTPATH%%SCENARIONAME%\INPUT\trn
+copy /Y %INPUTFARESPATH%\farelinks.far                         					%PROJECTPATH%%SCENARIONAME%\INPUT\trn
+copy /Y %INPUTFARESPATH%\Ferry.far                            					%PROJECTPATH%%SCENARIONAME%\INPUT\trn
+copy /Y %INPUTFARESPATH%\HSR.far                            					%PROJECTPATH%%SCENARIONAME%\INPUT\trn
+copy /Y %INPUTFARESPATH%\SMART.far                            					%PROJECTPATH%%SCENARIONAME%\INPUT\trn
+copy /Y %INPUTFARESPATH%\xfare.far                            					%PROJECTPATH%%SCENARIONAME%\INPUT\trn
+copy /Y %INPUTFARESPATH%\TransitSkims%SCENARIOINDEX%.job                       	%PROJECTPATH%%SCENARIONAME%\CTRAMP\scripts\skims
+copy /Y %INPUTFARESPATH%\overrideTransitSkimMatrix%SCENARIOINDEX%.job         	%PROJECTPATH%%SCENARIONAME%\CTRAMP\scripts\skims\overrideTransitSkimMatrix.job
+copy /Y %INPUTFARESPATH%\RestartRunIteration%SCENARIOINDEX%.bat 				%PROJECTPATH%%SCENARIONAME%\CTRAMP\RestartRunIteration.bat
+
+:: Move the input files, which are not accessed by the model, to the working directories
+copy /Y INPUT\trn\                 trn\
+
+@echo off
+FOR /f %%a IN ('WMIC OS GET LocalDateTime ^| FIND "."') DO SET DTS=%%a
+SET TimeStamp=%DTS:~0,4%%DTS:~4,2%%DTS:~6,2%%DTS:~8,2%%DTS:~10,2%%DTS:~12,2%
+echo %TimeStamp%
+
+:: The following environment variable allows the sample share to be varied from this scenario-specific file which is used to trigger the model run.  
+set MYSAMPLESHARE=0.5
+
+Call RestartRunModel.bat > %LOGPATH%%TimeStamp%RestartRunModel.txt
+
+::
