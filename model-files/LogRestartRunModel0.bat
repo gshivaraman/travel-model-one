22e3317b26eb39fa6bec7412f8cef02b22543b97
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2021-06-27 17:07:56 -0400

    parameterised the scenario index part of COMMPATH

--- model-files/LogRestartRunModel0.bat
+++ model-files/LogRestartRunModel0.bat
@@ -3,7 +3,7 @@
 SET SCENARIOINDEX=0
 
 :: The name of the scenario and the name of the scenario folder in which the model will be run.
-SET SCENARIONAME=2015_TM152_STR_S0
+SET SCENARIONAME=2015_TM152_STR_S%SCENARIOINDEX%
 
 :: The path to the project folder
 SET PROJECTPATH=F:\23791501\
