0df6d0507224079d91744873c304b8b4a3c2cb09
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2021-08-03 00:19:54 -0400

    Added test scripts.

---
+++ utilities/outputs/20210802_to_test_JoinSkimsStr.R
@@ -0,0 +1,20 @@
+
+library(tidyverse)
+library(openxlsx)
+
+source('JoinSkimsStr.R', encoding = 'UTF-8')
+
+trips <- JoinSkimsStr(fullrun=TRUE, iter=4, sampleshare=0.5, logrun=TRUE)
+
+trips <- JoinSkimsStr(fullrun=FALSE, iter=4, sampleshare=0.5, logrun=TRUE)
+
+summarize_attributes_mean <- trips %>%
+  group_by(skims_mode) %>%
+  summarize(walktime = mean(walktime), wait = mean(wait), IVT = mean(IVT), transfers = mean(transfers), boardfare = mean(boardfare), xfare = mean(xfare), faremat = mean(faremat)) %>%
+  ungroup()
+
+summarize_attributes_min <- trips %>%
+  group_by(skims_mode) %>%
+  summarize(walktime = min(walktime), wait = min(wait), IVT = min(IVT), transfers = min(transfers), boardfare = min(boardfare), xfare = min(xfare), faremat = min(faremat)) %>%
+  ungroup()
+
---
+++ utilities/outputs/20210802_to_test_SummariseStr.R
@@ -0,0 +1,7 @@
+
+library(tidyverse)
+library(openxlsx)
+
+source('SummariseStr.R', encoding = 'UTF-8')
+
+SummariseStr(sampleshare=0.5, logrun=TRUE)
