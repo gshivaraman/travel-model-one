672e75bd9de1cb513d6503cf01ec9d7b2abcf8a0
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2021-08-04 23:20:33 -0700

    Add files via upload

--- utilities/outputs/20210804_to_test_WorbookStr.R
+++ utilities/outputs/20210804_to_test_WorbookStr.R
@@ -9,6 +9,30 @@ setwd("G:/AMitrani/BAF_23791501")
 source('WorkbookStr.R')
 source('SummariseStr.R')
 
+setwd("2015_TM152_IPA_16_NB")
+
+WorkbookStr(scenariocode="2015_TM152_IPA_16_NB")
+
+print(gc())
+
+setwd("..")
+
+setwd("2015_TM152_IPA_16_T16")
+
+WorkbookStr(scenariocode="2015_TM152_IPA_16_T16")
+
+print(gc())
+
+setwd("..")
+
+setwd("2015_TM152_STR_S2")
+
+WorkbookStr(scenariocode="2015_TM152_STR_S2")
+
+print(gc())
+
+setwd("..")
+
 setwd("2015_TM152_STR_T8")
 
 WorkbookStr(scenariocode="2015_TM152_STR_T8")
@@ -17,4 +41,5 @@ print(gc())
 
 setwd("..")
 
-WorkbookStr()
+print(gc())
+
