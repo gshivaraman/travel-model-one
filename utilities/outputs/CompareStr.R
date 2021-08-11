770e37cd5b1b9597190292d7e06ccd9c191f2eeb
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2021-08-11 19:39:01 -0400

    Update CompareStr.R

--- utilities/outputs/CompareStr.R
+++ utilities/outputs/CompareStr.R
@@ -13,10 +13,11 @@ CompareStr <- function(workbook1, workbook2, outputworkbook) {
   trip_mode1 <- read_excel(workbook1, sheet = "trip_mode")
   trip_mode2 <- read_excel(workbook2, sheet = "trip_mode")
   
-  trip_mode <- trip_mode1 %>% left_join(trip_mode2, by=c("trip_mode"), suffix=c("1", "2"))
+  trip_mode <- trip_mode1 %>% full_join(trip_mode2, by=c("trip_mode"), suffix=c("1", "2"))
   
   trip_mode <- trip_mode %>% 
-    select("trip_mode", "trips1", "trips2",	"revenue1", "revenue2", "fare1", "fare2",	"num_participants1", "num_participants2", "walktime1", "walktime2", "wait1", "wait2", "IVT1", "IVT2", "transfers1", "transfers2", "othercost1", "othercost2", "distance1", "distance2", "dLocal1", "dLocal2", "dRegional1", "dRegional2", "dFree1", "dFree2", "dInterCity1", "dInterCity2", "ddist1", "ddist2", "dFareMat1", "dFareMat2")
+    select("trip_mode", "trips1", "trips2",	"revenue1", "revenue2", "fare1", "fare2",	"num_participants1", "num_participants2", "walktime1", "walktime2", "wait1", "wait2", "IVT1", "IVT2", "transfers1", "transfers2", "othercost1", "othercost2", "distance1", "distance2", "dLocal1", "dLocal2", "dRegional1", "dRegional2", "dFree1", "dFree2", "dInterCity1", "dInterCity2", "ddist1", "ddist2", "dFareMat1", "dFareMat2") %>%
+    arrange("trip_mode")
   
   # export the resulting table to Excel
   
@@ -34,10 +35,11 @@ CompareStr <- function(workbook1, workbook2, outputworkbook) {
     mydf1 <- read_excel(workbook1, sheet = mysheetname)
     mydf2 <- read_excel(workbook2, sheet = mysheetname)
     
-    mydf <- mydf1 %>% left_join(mydf2, by=c(all_of(myvar), "trip_mode"), suffix=c("1", "2"))
+    mydf <- mydf1 %>% full_join(mydf2, by=c(all_of(myvar), "trip_mode"), suffix=c("1", "2"))
     
     mydf <- mydf %>% 
-      select(all_of(myvar), "trip_mode", "trips1", "trips2",	"revenue1", "revenue2", "fare1", "fare2",	"num_participants1", "num_participants2", "walktime1", "walktime2", "wait1", "wait2", "IVT1", "IVT2", "transfers1", "transfers2", "othercost1", "othercost2", "distance1", "distance2", "dLocal1", "dLocal2", "dRegional1", "dRegional2", "dFree1", "dFree2", "dInterCity1", "dInterCity2", "ddist1", "ddist2", "dFareMat1", "dFareMat2")
+      select(all_of(myvar), "trip_mode", "trips1", "trips2",	"revenue1", "revenue2", "fare1", "fare2",	"num_participants1", "num_participants2", "walktime1", "walktime2", "wait1", "wait2", "IVT1", "IVT2", "transfers1", "transfers2", "othercost1", "othercost2", "distance1", "distance2", "dLocal1", "dLocal2", "dRegional1", "dRegional2", "dFree1", "dFree2", "dInterCity1", "dInterCity2", "ddist1", "ddist2", "dFareMat1", "dFareMat2") %>%
+      arrange(all_of(myvar), "trip_mode")
     
     
     if(myvar == "incQ") {
