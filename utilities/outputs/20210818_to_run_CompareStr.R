663694be6860217fcb9a5050de72a2323fd1aa92
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2021-08-18 20:34:40 -0400

    Add files via upload

--- utilities/outputs/20210818_to_run_CompareStr.R
+++ utilities/outputs/20210818_to_run_CompareStr.R
@@ -10,8 +10,24 @@ source('CompareStr.R')
 
 setwd("G:/AMitrani/BAF_23791501/Output workbooks")
 
-CompareStr(workbook1 = "WorkbookStr_2015_TM152_IPA_16_NB_20210817191457amitrani.xlsx", workbook2 = "WorkbookStr_2015_TM152_IPA_16_T16_20210817231416amitrani.xlsx", outputworkbook = "WorkbookStr_2015_TM152_IPA_16_T16_20210817231416amitrani_vs_BAU.xlsx")
+# ETG base vs ETG tests
 
-CompareStr(workbook1 = "WorkbookStr_2015_TM152_IPA_16_NB_20210817191457amitrani.xlsx", workbook2 = "WorkbookStr_2015_TM152_IPA_16_T17_20210818003136amitrani.xlsx", outputworkbook = "WorkbookStr_2015_TM152_IPA_16_T17_20210818003136amitrani_vs_BAU.xlsx")
+CompareStr(workbook1 = "WorkbookStr_2015_TM152_IPA_16_NB_20210818144759amitrani", workbook2 = "WorkbookStr_2015_TM152_IPA_16_T3_20210818153423amitrani")
+
+CompareStr(workbook1 = "WorkbookStr_2015_TM152_IPA_16_NB_20210818144759amitrani", workbook2 = "WorkbookStr_2015_TM152_IPA_16_T16_20210818154552amitrani")
+
+CompareStr(workbook1 = "WorkbookStr_2015_TM152_IPA_16_NB_20210818144759amitrani", workbook2 = "WorkbookStr_2015_TM152_IPA_16_T17_20210818155720amitrani")
+
+# Steer base vs Steer tests
+
+CompareStr(workbook1 = "WorkbookStr_2015_TM152_STR_S2_20210818160843amitrani", workbook2 = "WorkbookStr_2015_TM152_STR_T8_20210818162014amitrani")
+
+CompareStr(workbook1 = "WorkbookStr_2015_TM152_STR_S2_20210818160843amitrani", workbook2 = "WorkbookStr_2015_TM152_STR_T14_20210818163147amitrani")
+
+CompareStr(workbook1 = "WorkbookStr_2015_TM152_STR_S2_20210818160843amitrani", workbook2 = "WorkbookStr_2015_TM152_STR_T20_20210818164322amitrani")
+
+# ETG base versus Steer base
+
+CompareStr(workbook1 = "WorkbookStr_2015_TM152_IPA_16_NB_20210818144759amitrani", workbook2 = "WorkbookStr_2015_TM152_STR_S2_20210818160843amitrani")
 
 #
