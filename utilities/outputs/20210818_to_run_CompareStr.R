feb3e17b3758cd435dcdec3295513809d75dd296
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2021-09-01 16:37:10 -0400

    Add files via upload

--- utilities/outputs/20210818_to_run_CompareStr.R
+++ utilities/outputs/20210818_to_run_CompareStr.R
@@ -42,6 +42,8 @@ CompareStr(workbook1 = "WorkbookStr_2015_TM152_STR_S2_20210818160843amitrani.xls
 
 CompareStr(workbook1 = "WorkbookStr_2015_TM152_STR_S2_20210818160843amitrani.xlsx", workbook2 = "WorkbookStr_2015_TM152_STR_T24_20210830103758amitrani.xlsx")
 
+CompareStr(workbook1 = "WorkbookStr_2015_TM152_STR_S2_20210818160843amitrani.xlsx", workbook2 = "WorkbookStr_2015_TM152_STR_T25_20210831124830amitrani.xlsx")
+
 # ETG base versus Steer base
 
 CompareStr(workbook1 = "WorkbookStr_2015_TM152_IPA_16_NB_20210818144759amitrani.xlsx", workbook2 = "WorkbookStr_2015_TM152_STR_S2_20210818160843amitrani.xlsx")
