f6b796737ee56b135ee97826c504eb4fc393636b
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2021-09-10 20:16:04 -0400

    Add files via upload

---
+++ utilities/outputs/20210910_ProduceOutputsStr_2015_TM152_STR_T43.R
@@ -0,0 +1,10 @@
+
+
+setwd("G:/AMitrani/BAF_23791501")
+
+source("ProduceOutputsStr.R", encoding = "UTF-8")
+
+ProduceOutputsStr(myrunfoldername="2015_TM152_STR_T43", fullrun=TRUE, iter=4, sampleshare=0.5, logrun=TRUE, baufares=FALSE)
+
+#
+
